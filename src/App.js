10ce90e2654ff51a376fb9574c7e5a07bb5e5f95
sky0815a <sky0815a@gmail.com>
2022-11-16 17:03:15 -0500

    Calling Data Workingg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios'
 
 function App() {
+  const [data,setData] = useState({})
+  const [location, setLocation] = useState('')
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=99ea53a733d2dfeb06d4511846b3dda8`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='search'>
+        <input
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Enter Location'
+        type='text'/>
+      </div>
+       
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          {/* F = (K - 273.15) * 1.8 + 32 */}
+          <div className="temp">
+            {data.main ? <h1>{Math.round((data.main.temp-273.15)*1.8+32)}F</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+            <p>Feels Like</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            {data.main ? <p>{data.main.feels_like}F</p> : null}
+          </div>
+          <div className='humidity'>
+            {data.main ? <p>{data.main.humidity}%</p> : null}
+            <p>humidity</p>
+          </div>
+          <div className='wind'>
+            {data.wind ? <p>{data.wind.speed}MPH</p> : null}
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
     </div>
+    
   );
 }
 
 export default App;
+
+
+// Reference 33min
+// https://www.youtube.com/watch?v=UjeXpct3p7M&ab_channel=CodeCommerce
